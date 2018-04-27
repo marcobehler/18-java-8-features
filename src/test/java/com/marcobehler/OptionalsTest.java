8c4ce4633e596aa7f4e5b04c674c2de994ad0b6c
Marco Behler <marco@marcobehler.com>
2018-04-27 09:25:03 +0200

    potus test

--- src/test/java/com/marcobehler/OptionalsTest.java
+++ src/test/java/com/marcobehler/OptionalsTest.java
@@ -1,9 +1,5 @@
 package com.marcobehler;
 
-import org.junit.Test;
-
-import java.util.Optional;
-
 /**
  * Thanks for watching this episode! If you want to, drop me a line to info@marcobehler.com.
  */
