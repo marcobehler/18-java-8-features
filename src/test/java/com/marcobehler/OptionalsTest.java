b09af6bbc5e3bbd8cc8d26a98166c757a3bd3394
Marco Behler <marco@marcobehler.com>
2018-04-30 16:05:13 +0200

    potus test

--- src/test/java/com/marcobehler/OptionalsTest.java
+++ src/test/java/com/marcobehler/OptionalsTest.java
@@ -23,5 +23,54 @@ public class OptionalsTest {
         );*/
     }
 
+    @Test
+    public void optionals_part_2() {
+        Potus anyPotus = new Potus();
 
+        Optional<Wife> wife = findWife(anyPotus, "M");
+
+        String name = wife
+                .filter(w -> w.getName().endsWith("a"))
+                .map(w -> w.getName().toUpperCase())
+                .orElse("I DoNT hAVE A nAME");
+
+        System.out.println("name = " + name);
+    }
+
+
+    @Test
+    public void exercise() {
+      //  String name = new Potus2().flatMap();
+        //System.out.println("name = " + name);
+    }
+
+    public static class Potus2 {
+        private final Optional<Pet> pet;
+
+        public Potus2(Optional<Pet> pet) {
+            this.pet = pet;
+        }
+
+        public Optional<Pet> getPet() {
+            return pet;
+        }
+    }
+
+    public static class Pet {
+        private final Optional<String> name;
+
+        public Pet(Optional<String> name) {
+            this.name = name;
+        }
+
+        public Optional<String> getName() {
+            return name;
+        }
+    }
+
+    private Optional<Wife> findWife(Potus anyPotus, String startingLetter) {
+        return anyPotus.getWives().stream()
+                .filter(w -> w.getName().startsWith(startingLetter))
+                .findAny();
+    }
 }
